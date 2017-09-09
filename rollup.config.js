d94be9d32e94e870f12132c2bc841635f00f1a49
Tyler Torola <tyler@tjt.codes>
2017-09-09 09:01:12 -0700

    Fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import resolve from "rollup-plugin-node-resolve"
+import babel from "rollup-plugin-babel"
 
 export default {
   input: "src/main.js",
   output: {
     file: "build/crossed.js",
-    format: "cjs"
+    format: "iife"
   },
-  plugins: [resolve()]
+  plugins: [babel()]
 }
