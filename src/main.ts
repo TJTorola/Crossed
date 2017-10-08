cbe5333b8a10920920131f589b36ec3c4d4d1970
Tyler Torola <tyler@tjt.codes>
2017-10-08 10:56:15 -0700

    Update main function for more abstraction

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-export default () => {
-  ReactDOM.render(
-    React.createElement("div", null, "hello world"),
-    document.getElementById("app")
-  )
+const REACT_NOT_LOADED = `
+React & ReactDOM must be loaded into global state for Crossed to function
+`
+
+const NO_MOUNT_ID = `
+A mounting ID must be provided to mount Crossed into the webpage
+`
+
+const NO_ELEMENT_FOUND = mountID => `
+No element was found for the given mountID: ${mountID}
+`
+
+export default mountID => {
+  if (!React || !ReactDOM) throw new Error(REACT_NOT_LOADED)
+  if (!mountID) throw new Error(NO_MOUNT_ID)
+
+  const mountElement = document.getElementById(mountID)
+
+  if (!mountElement) throw new Error(NO_ELEMENT_FOUND(mountID))
+  ReactDOM.render(React.createElement("div", null, "hello world"), mountElement)
 }
