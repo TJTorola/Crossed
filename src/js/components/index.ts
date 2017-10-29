ad375316d331b36d0ee1849a1c236fca1974e660
Tyler Torola <Tyler@TJT.Codes>
2017-10-28 18:11:40 -0700

    Refactor picodux (#5)

--- src/js/components/index.ts
+++ src/js/components/index.ts
@@ -1,21 +1,33 @@
 import { h } from "picodom/src"
+import { connect } from "../lib/picodux"
 
-export default (state, dispatch) =>
+const App = ({ incrament, decrament, value }) =>
   h("main", {}, [
-    h("h1", {}, state),
+    h("h1", {}, value),
     h(
       "button",
       {
-        disabled: state <= 0,
-        onclick: () => dispatch({ type: "DECRAMENT" })
+        disabled: value <= 0,
+        onclick: decrament
       },
       "–"
     ),
     h(
       "button",
       {
-        onclick: () => dispatch({ type: "INCRAMENT" })
+        onclick: incrament
       },
       "+"
     )
   ])
+
+const mapStateToProps = state => ({
+  value: state
+})
+
+const mapDispatchToProps = dispatch => ({
+  incrament: () => dispatch({ type: "INCRAMENT" }),
+  decrament: () => dispatch({ type: "DECRAMENT" })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
